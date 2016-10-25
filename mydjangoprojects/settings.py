ea1e0ced14b2526f5d2eb05ec8443b9274bb565c
dyah <sukmadyu@gmail.com>
2016-10-25 11:26:14 +0700

    polls app added

--- mydjangoprojects/settings.py
+++ mydjangoprojects/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
     'portfolio',
     'blog',
     'todo',
-    'polls',
+
     'gallery',
     'library',
 ]
