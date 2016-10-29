a1fc11f26e657e84749b302cb65addfb88074950
dyah <sukmadyu@gmail.com>
2016-10-29 11:33:42 +0700

    gallery updated

--- gallery/admin.py
+++ gallery/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Gallery
 
 # Register your models here.
-admin.site.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Gallery, GalleryAdmin)
+
